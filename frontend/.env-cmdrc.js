9d01ae608c4dd5ab8e17d00ab90f251b1ee78730
Akshay <araa@connect.ust.hk>
2021-08-11 17:56:55 +0530

    Update to use env vars to decide which api url to use

---
+++ frontend/.env-cmdrc.js
@@ -0,0 +1,22 @@
+// NOTE: In order for these variables to be read by the react app you need to prefix them with "REACT_APP_"
+const PROD = {
+  REACT_APP_ENVIRONMENT: "Production",
+  REACT_APP_API_ENDPOINT:
+    "https://contract-parser-api-7s4aptchzq-uc.a.run.app/api/",
+};
+
+const DEV = {
+  REACT_APP_ENVIRONMENT: "Development",
+  REACT_APP_API_ENDPOINT:
+    "https://contract-parser-api-s27o6wzppa-uc.a.run.app/",
+};
+const LOCAL = {
+  REACT_APP_ENVIRONMENT: "Local",
+  REACT_APP_API_ENDPOINT: "http://localhost:3000/api/",
+};
+
+module.exports = {
+  prod: PROD,
+  dev: DEV,
+  local: LOCAL,
+};
